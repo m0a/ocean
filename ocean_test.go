e7b2c7ed7f98626c26962a0a2d8904b3ff4b906d
Abe Makoto <abe00makoto@gmail.com>
2014-05-07 16:42:39 +0900

    test1

---
+++ ocean_test.go
@@ -0,0 +1,16 @@
+package ocean
+
+import (
+	"testing"
+)
+
+func TestOcean(t *testing.T) {
+
+	if true {
+		t.Errorf("test ocean error")
+	}
+}
+
+func TestOcean2(t *testing.T) {
+	t.Error("test ocean2 error")
+}
